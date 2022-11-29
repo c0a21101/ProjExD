c8c41bdca77a7b35f35a6899779160e87ea54a21
Kenta <c0a211015d@edu.teu.ac.jp>
2022-11-29 14:08:46 +0900

    練習問題3 クリックに対する反応の定義

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,4 +1,11 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
+
+
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showinfo(txt, f"{txt}のボタンが押されました")
 
 root = tk.Tk()
 root.geometry("300x500")
@@ -9,7 +16,8 @@ for i in range(10):
                         text = str(i),
                         width = "4",
                         height = "2",
-                        font = ("", 30),)
+                        font = ("", 30))
     button[i].grid(row = 3 - (i+2)//3, column = 3 - (i-1)%3)
+    button[i].bind("<1>", button_click)
 
 root.mainloop()
