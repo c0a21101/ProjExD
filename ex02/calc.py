1fd2ef45db9f3037f3dbf84c42a882fe05d8a1d5
Kenta <c0a211015d@edu.teu.ac.jp>
2022-11-29 15:30:16 +0900

    四則演算の追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -10,10 +10,14 @@ def button_click(event):
         entry.delete(0, tk.END)
         entry.insert(tk.END, eval(formula))
     else:
+        if txt == "×":
+            txt = "*"
+        if txt == "÷":
+            txt = "/"
         entry.insert(tk.END, txt)
 
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("400x600")
 
 button_num = [0] * 10
 for i in range(len(button_num)):
@@ -22,10 +26,13 @@ for i in range(len(button_num)):
                               width = "4",
                               height = "2",
                               font = ("", 30))
-    button_num[i].grid(row = 4 - (i+2)//3, column = 2 - (i-1)%3)
+    if i != 0:
+        button_num[i].grid(row = 5 - (i+2)//3, column = (i-1)%3)
+    else:
+        button_num[i].grid(row = 5, column = 1)
     button_num[i].bind("<1>", button_click)
 
-operators = ["+", "="]
+operators = ["+", "-", "×", "÷", "="]
 button_ope = [0] * len(operators)
 for i in range(len(operators)):
     button_ope[i] = tk.Button(root,
@@ -33,12 +40,12 @@ for i in range(len(operators)):
                               width = "4",
                               height = "2",
                               font = ("", 30))
-    button_ope[i].grid(row = 4, column = 1 + i)
+    button_ope[i].grid(row = i+1, column = 3)
     button_ope[i].bind("<1>", button_click)
 
 entry = tk.Entry(justify = "right",
-                 width = 30,
+                 width = 42,
                  font = 40)
-entry.grid(row = 0, column = 0, columnspan = 3)
+entry.grid(row = 0, column = 0, columnspan = 4)
 
 root.mainloop()
