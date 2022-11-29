e64c00584b4fbb968df5049c2a1414244cc57c6e
Kenta <c0a211015d@edu.teu.ac.jp>
2022-11-29 14:47:44 +0900

    練習問題5 +,=ボタンの追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -20,6 +20,16 @@ for i in range(len(button_num)):
     button_num[i].grid(row = 4 - (i+2)//3, column = 2 - (i-1)%3)
     button_num[i].bind("<1>", button_click)
 
+operators = ["+", "="]
+button_ope = [0] * len(operators)
+for i in range(len(operators)):
+    button_ope[i] = tk.Button(root,
+                              text = operators[i],
+                              width = "4",
+                              height = "2",
+                              font = ("", 30))
+    button_ope[i].grid(row = 4, column = 1 + i)
+
 entry = tk.Entry(justify = "right",
                  width = 30,
                  font = 40)
