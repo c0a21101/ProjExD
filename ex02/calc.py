9f6b7f4bbb52e7b79cd5f7f09f7d17c1f6a3213e
Kenta <c0a211015d@edu.teu.ac.jp>
2022-11-29 17:07:44 +0900

    コメントの追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,15 +1,17 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-
+#ボタンを押された時の挙動
 def button_click(event):
     btn = event.widget
     txt = btn["text"]
+    # = の時
     if txt == "=":
         formula = entry.get()
         entry.delete(0, tk.END)
         entry.insert(tk.END, eval(formula))
     else:
+        #四則演算の時
         if txt in operators:
             if entry.get()[-1].isdecimal() == True:
                 if txt == "×":
@@ -17,6 +19,7 @@ def button_click(event):
                 if txt == "÷":
                     txt = "/"
                 entry.insert(tk.END, txt)
+        #削除系の時
         elif txt in deletes:
             if txt == "←":
                 entry.delete(len(entry.get())-1, tk.END)
@@ -31,26 +34,31 @@ def button_click(event):
                         break
                 entry.delete(not_num, tk.END)
             entry.insert(0, 0)
+        #小数点の時
         elif txt == ".":
             not_num = 0
             for i in range(len(entry.get())-1, 0, -1):
                 if entry.get()[i].isdecimal() == False:
                     if entry.get()[i].isdecimal() != ".":
-                        not_num = i-1
+                        not_num = i
                         break
             if not "." in entry.get()[not_num:]:
                 if len(entry.get()[not_num:]) > 0:
                     entry.insert(tk.END, txt)
+        #その他（数字）の時
         else:
             entry.insert(tk.END, txt)
+        #頭に余分な0があったら消す
         if len(entry.get()) > 1:
             if entry.get()[0] == "0":
                 if entry.get()[1] != ".":
                     entry.delete(0, 1)
 
+#初期設定
 root = tk.Tk()
 root.geometry("400x600")
 
+#各種ボタンの作成
 button_num = [0] * 10
 for i in range(len(button_num)):
     button_num[i] = tk.Button(root,
@@ -94,6 +102,7 @@ for i in range(len(deletes)):
     button_del[i].grid(row = 1, column = i)
     button_del[i].bind("<1>", button_click)
 
+#出力部分
 entry = tk.Entry(justify = "right",
                  width = 42,
                  font = 40)
