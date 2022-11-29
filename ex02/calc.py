ee81d0af43707bf1be78dc245837652ad870f8f3
Kenta <c0a211015d@edu.teu.ac.jp>
2022-11-29 14:50:00 +0900

    練習問題6 ボタンクリック時の挙動の変更

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,7 +5,7 @@ import tkinter.messagebox as tkm
 def button_click(event):
     btn = event.widget
     txt = btn["text"]
-    tkm.showinfo(txt, f"{txt}のボタンが押されました")
+    entry.insert(tk.END, txt)
 
 root = tk.Tk()
 root.geometry("300x500")
@@ -29,6 +29,8 @@ for i in range(len(operators)):
                               height = "2",
                               font = ("", 30))
     button_ope[i].grid(row = 4, column = 1 + i)
+    if operators[i] != "=":
+        button_ope[i].bind("<1>", button_click)
 
 entry = tk.Entry(justify = "right",
                  width = 30,
