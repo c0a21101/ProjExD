c1dfbb5ed4dc2211619e99b543cde670f80606f3
Kenta <c0a211015d@edu.teu.ac.jp>
2022-11-29 14:04:07 +0900

    練習問題2 ボタン作成

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,4 +3,13 @@ import tkinter as tk
 root = tk.Tk()
 root.geometry("300x500")
 
+button = [0] * 10
+for i in range(10):
+    button[i] = tk.Button(root,
+                        text = str(i),
+                        width = "4",
+                        height = "2",
+                        font = ("", 30),)
+    button[i].grid(row = 3 - (i+2)//3, column = 3 - (i-1)%3)
+
 root.mainloop()
