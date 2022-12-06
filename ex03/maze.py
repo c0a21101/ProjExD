d15d309916f1a7509aaf83601fbb85d8f6c38b1e
Kenta <c0a211015d@edu.teu.ac.jp>
2022-12-06 14:00:53 +0900

    練習3 こうかとんの画像の表示

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,12 +2,18 @@ import tkinter as tk
 
 
 if __name__ == "__main__":
-    #ウィンドウ生成
+    # ウィンドウ生成
     root = tk.Tk()
     root.title("迷えるこうかとん")
 
-    #背景が黒色のCanvasを生成
+    # 背景が黒色のCanvasを生成
     canvas = tk.Canvas(width=1500,height=900,bg="black")
+
+    # こうかとんの画像を表示
+    cx = 300  # こうかとんの横軸の現在地
+    cy = 400  # こうかとんの縦軸の現在地
+    image = tk.PhotoImage(file="fig/0.png")
+    canvas.create_image(cx,cy,image=image)
     canvas.pack()
 
     root.mainloop()
