56afdd263e6a3859238d39710e826ca59715d9d7
Kenta <c0a211015d@edu.teu.ac.jp>
2022-12-06 16:36:02 +0900

    スタート地点のランダム生成とそこから一番遠いところをゴールにするように

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 import maze_maker
+import random
+import copy
+
 
 
 #リアルタイム処理関数
@@ -21,7 +24,6 @@ def main_proc():
         timer = 0
     else:
         timer += 1
-    print(timer)
     
     # 座標の更新
     cx = mx * 100 + 50
@@ -37,6 +39,29 @@ def main_proc():
     else:
         canvas.create_image(cx,cy,image=images[0],tag="player")
     root.after(50, main_proc)
+
+
+# ゴール生成アルゴリズム
+def set_goal(x, y, id = 0):
+    global far, queue, gx, gy
+    if far[x][y-1] == 0:
+        queue += [[x, y-1]]
+        far[x][y-1] = 1
+    if far[x][y+1] == 0:
+        queue += [[x, y+1]]
+        far[x][y+1] = 1
+    if far[x-1][y] == 0:
+        queue += [[x-1, y]]
+        far[x-1][y] = 1
+    if far[x+1][y] == 0:
+        queue += [[x+1, y]]
+        far[x+1][y] = 1
+    if id < len(queue)-1:
+        set_goal(queue[id+1][0], queue[id+1][1], id+1)
+    else:
+        gx = x
+        gy = y
+
 
 
 def key_down(event):
@@ -61,9 +86,21 @@ if __name__ == "__main__":
     maze = maze_maker.make_maze(15, 9)
     maze_maker.show_maze(canvas, maze)
 
+    # スタート地点の設定
+    sx = random.randrange(1,15,2)
+    sy = random.randrange(1,9,2)
+    canvas.create_rectangle(sx*100, sy*100, sx*100+100, sy*100+100, fill="blue")
+
+    # ゴール地点の設定
+    far = copy.deepcopy(maze)
+    far[sx][sy] = 1
+    queue = [[sx, sy]]
+    set_goal(queue[0][0], queue[0][1])
+    canvas.create_rectangle(gx*100, gy*100, gx*100+100, gy*100+100, fill="green")
+
     # こうかとんの画像を表示
-    mx = 1  # こうかとんの横軸のマス
-    my = 1  # こうかとんの縦軸のマス
+    mx = sx  # こうかとんの横軸のマス
+    my = sy  # こうかとんの縦軸のマス
     cx = mx * 100 + 50  # こうかとんの横軸の座標
     cy = my * 100 + 50  # こうかとんの縦軸の座標
     images = [tk.PhotoImage(file="fig/0.png"),
