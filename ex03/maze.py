5a2d1bef0f99c5e074cea8e28918aaff84a41fd5
Kenta <c0a211015d@edu.teu.ac.jp>
2022-12-06 13:51:13 +0900

    練習2 背景色が黒のCanvas生成

--- ex03/maze.py
+++ ex03/maze.py
@@ -6,4 +6,8 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
 
+    #背景が黒色のCanvasを生成
+    canvas = tk.Canvas(width=1500,height=900,bg="black")
+    canvas.pack()
+
     root.mainloop()
