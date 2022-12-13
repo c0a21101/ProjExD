46db5708425e1618dcf95256d2b0623bdec79e1d
Kenta <c0a211015d@edu.teu.ac.jp>
2022-12-13 14:31:32 +0900

    kadai4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -13,22 +13,38 @@ def main():
     bg_sfc = pg.image.load("fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
     bg_rct.center = 800, 450
-    scrn_sfc.blit(bg_sfc, bg_rct)
 
     # 操作キャラ（こうかとん）生成
     tori_sfc = pg.image.load("fig/6.png")
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 800, 450
-    scrn_sfc.blit(tori_sfc, tori_rct)
 
-    pg.display.update()
-    clock.tick(1000)  # 1000fpsの時を刻む
 
     while True:
+        # 各種オブジェクトの描画
+        scrn_sfc.blit(bg_sfc, bg_rct)
+        scrn_sfc.blit(tori_sfc, tori_rct)
+
+        
+        
+        # イベントを繰り返しで処理
         for event in pg.event.get():
             if event.type == pg.QUIT:  # 「×」ボタンが押されたらウィンドウを閉じる
                 return
+
+        key_dct = pg.key.get_pressed()
+        if key_dct[pg.K_UP]:
+            tori_rct.centery -= 1
+        if key_dct[pg.K_DOWN]:
+            tori_rct.centery += 1
+        if key_dct[pg.K_LEFT]:
+            tori_rct.centerx -= 1
+        if key_dct[pg.K_RIGHT]:
+            tori_rct.centerx += 1
+
+        pg.display.update()
+        clock.tick(1000)  # 1000fpsの時を刻む
 
 
 if __name__ == "__main__":
